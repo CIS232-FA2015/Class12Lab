414b4b18d8a579f12744574b512c086889daaa2b
SK207_inst <SK207_inst@K207_W8Inst.carrollcc.edu>
2015-10-08 19:43:01 -0400

    Solution to class 12 Lab

--- Class12Lab/src/cis232/classlab/SecretDecoder.java
+++ Class12Lab/src/cis232/classlab/SecretDecoder.java
@@ -1,10 +1,23 @@
 package cis232.classlab;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class SecretDecoder {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		System.out.println("What is the key?");
+		int key = new Scanner(System.in).nextInt();
 		
-
+		DataInputStream dis = new DataInputStream(new FileInputStream("secret.msg"));
+		String encMessage = dis.readUTF();
+		dis.close();
+		
+		for(int i = 0; i < encMessage.length(); i++){
+			System.out.print((char)(encMessage.charAt(i) - key));
+		}
 	}
 
 }
